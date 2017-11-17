8d2530aa23d16d98a4bcce610d24e083bf3f3a50
Andrey Matrosov <matrosovs2008@yandex.ru>
2017-11-17 15:08:23 +0300

    add keyBoard and hidden kb

--- src/main/java/by/matrosov/TelegramBot.java
+++ src/main/java/by/matrosov/TelegramBot.java
@@ -5,9 +5,13 @@ import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.PhotoSize;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -29,7 +33,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             else if(update.hasMessage() && update.getMessage().hasPhoto()){
 
                 List<PhotoSize> photos = message.getPhoto();
-                
+
                 String f_id = photos.stream()
                         .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
                         .findFirst()
@@ -63,6 +67,14 @@ public class TelegramBot extends TelegramLongPollingBot {
             }else if (message_txt.equals("/pic")){
 
                 sendReply(message,"https://cs5.pikabu.ru/images/big_size_comm/2015-11_3/1447440183173454809.jpg");
+
+            }else if (message_txt.equals("/markup")){
+
+                sendMsgWithKeyboard(message,"Magic");
+
+            }else if (message_txt.equals("/hide")){
+
+                sendMsgWithHiddenKeyBoard(message,"keyboard hidden");
             }
         }
     }
@@ -84,4 +96,64 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void sendMsgWithKeyboard(Message message, String text){
+        SendMessage s = new SendMessage();
+        s.setChatId(message.getChatId());
+        s.setText(text);
+
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        s.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Комманда 1");
+        keyboardFirstRow.add("Комманда 2");
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add("Комманда 3");
+        keyboardSecondRow.add("Комманда 4");
+        keyboardSecondRow.add("Комманда 5");
+
+        // 3 строчка клавиатуры
+        KeyboardRow keyboardThirdRow = new KeyboardRow();
+        keyboardThirdRow.add("Какая длинная кнопка");
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        keyboard.add(keyboardThirdRow);
+
+        // устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+        try {
+            execute(s);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMsgWithHiddenKeyBoard(Message message,String text){
+        SendMessage s = new SendMessage();
+        s.setChatId(message.getChatId());
+        s.setText(text);
+
+        ReplyKeyboardRemove keyboardMarkup = new ReplyKeyboardRemove();
+        s.setReplyMarkup(keyboardMarkup);
+        try {
+            execute(s);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
